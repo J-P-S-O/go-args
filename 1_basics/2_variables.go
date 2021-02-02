4ea25bfb59cc7eaae26569fb3fc4bdfd085bae77
J-p-s-o <jpso.guerreiro@gmail.com>
2021-02-02 10:02:24 -0300

    ,

---
+++ 1_basics/2_variables.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+"fmt"
+) // You can import like that also
+
+func main(){
+// with var.
+// Notice the default value
+var (
+	java string // " "
+	swift int // 0
+        cpp bool // false
+	
+)
+// parallel
+a, b, c := 1,2,3 // notice := symbol. Also note the automatic type inference
+
+}
