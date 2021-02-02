f2198c26760ab3575ed36eab49f640f2fb8cad83
J-p-s-o <jpso.guerreiro@gmail.com>
2021-02-02 10:06:42 -0300

    .

--- 1_basics/2_variables.go
+++ 1_basics/2_variables.go
@@ -7,12 +7,19 @@ import (
 func main(){
 // with var.
 // Notice the default value
+
 var (
 	java string // " "
 	swift int // 0
         cpp bool // false
 	
 )
+// can also be like that:
+var x string;
+// or 
+var y string = " "
+// or without types 
+var z = 0
 // parallel
 a, b, c := 1,2,3 // notice := symbol. Also note the automatic type inference
 
